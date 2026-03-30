46972c90d58e97af90880142f146814c5aafaad1
Axita <patelaxita75@gmail.com>
2026-03-30 13:02:18 +0530

    30/3 productRoutes

---
+++ Backend/routes/productRoutes.js
@@ -0,0 +1,331 @@
+const express = require('express');
+const { body, param } = require('express-validator');
+const router = express.Router();
+const productController = require('../controllers/productController');
+const validate = require('../middleware/validationMiddleware');
+const auth = require("../middleware/authMiddleware");
+
+/**
+ * @swagger
+ * tags:
+ *   name: Products
+ *   description: Product CRUD API
+ */
+
+/**
+ * @swagger
+ * /products:
+ *   get:
+ *     summary: Get all products
+ *     tags: [Products]
+ *     security:
+ *       - bearerAuth: []
+ *     responses:
+ *       200:
+ *         description: List of products
+ *       400:
+ *         description: invalid request
+ *       401:
+ *         description: unauthorized access
+ *       404:
+ *         description: Product not found
+ *       500:
+ *         description: Internal server error
+ */
+router.get('/products',auth, productController.getProducts);
+
+/**
+ * @swagger
+ * /products/{id}/{name}:
+ *   get:
+ *     summary: Get product by ID
+ *     tags: [Products]
+ *     security:
+ *       - bearerAuth: []
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         description: Product ID
+ *     responses:
+ *       200:
+ *         description: Product found
+ *       404:
+ *         description: Product not found
+ *       400:
+ *         description: invalid request
+ *       401:
+ *         description: unauthorized access
+ *       500:
+ *         description: Internal server error
+ */
+router.get(
+  '/products/:id/:name',
+  param('id').isInt().withMessage('Product ID must be integer'),
+  validate,auth,
+  productController.getProductById
+);
+
+/**
+ * @swagger
+ * /products:
+ *   post:
+ *     summary: Create new product
+ *     tags: [Products]
+ *     security:
+ *       - bearerAuth: []
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             required:
+ *               - name
+ *               - amount
+ *               - stock
+ *             properties:
+ *               name:
+ *                 type: string
+ *                 example: Aloe Vera Gel
+ *               amount:
+ *                 type: number
+ *                 example: 199
+ *               suggestedFor:
+ *                type: string
+ *                example: Skin hydration and soothing
+ *               ingredients:
+ *                type: string
+ *                example: Aloe vera extract, glycerin, water
+ *               stock:
+ *                 type: integer
+ *                 example: 50
+ *               description:
+ *                 type: string
+ *                 example: Pure aloe vera soothing gel
+ *               status:
+ *                 type: string
+ *                 example: Available
+ *               action:
+ *                 type: string
+ *                 example: Active
+ *     responses:
+ *       201:
+ *         description: Product created
+ *       400:
+ *         description: invalid request
+ *       401:
+ *         description: unauthorized access
+ *       404:
+ *         description: Product not found
+ *       500:
+ *         description: Internal server error
+ */
+router.post(
+  '/products',
+  body('name')
+    .notEmpty().withMessage('Name is required')
+    .isLength({ min: 3 }).withMessage('Name must be at least 3 characters'),
+
+  body('amount')
+    .notEmpty().withMessage('Amount is required')
+    .isFloat({ gt: 0 }).withMessage('Amount must be greater than 0'),
+
+  body('suggestedFor')
+    .optional()
+    .isLength({ min: 3 }).withMessage('Suggested For must be at least 3 characters'),
+
+  body('ingredients')
+    .optional()
+    .isLength({ min: 3 }).withMessage('Ingredients must be at least 3 characters'),
+
+  body('stock')
+    .notEmpty().withMessage('Stock is required')
+    .isInt({ min: 0 }).withMessage('Stock must be 0 or more'),
+
+  body('description')
+    .optional()
+    .isLength({ min: 5 }).withMessage('Description must be at least 5 characters'),
+
+  body('status')
+    .optional()
+    .isIn(['Available', 'Out of Stock'])
+    .withMessage('Status must be Available or Out of Stock'),
+
+  body('action')
+    .optional()
+    .isIn(['Active', 'Inactive'])
+    .withMessage('Action must be Active or Inactive'),
+
+  validate,auth,
+  productController.createProduct
+);
+
+/**
+ * @swagger
+ * /products/{id}:
+ *   put:
+ *     summary: Update product
+ *     tags: [Products]
+ *     security:
+ *       - bearerAuth: []
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               name:
+ *                 type: string
+ *               amount:
+ *                 type: number
+ *               suggestedFor:
+ *                 type: string
+ *               ingredients:
+ *                type: string
+ *               stock:
+ *                 type: integer
+ *               description:
+ *                 type: string
+ *               status:
+ *                 type: string
+ *               action:
+ *                 type: string
+ *     responses:
+ *       200:
+ *         description: Product updated
+ *       400:
+ *         description: invalid request
+ *       401:
+ *         description: unauthorized access
+ *       404:
+ *         description: Product not found
+ *       500:
+ *         description: Internal server error
+ */
+router.put(
+  '/products/:id',
+  param('id').isInt().withMessage('Product ID must be integer'),
+
+  body('name')
+    .optional()
+    .isLength({ min: 3 }).withMessage('Name must be at least 3 characters'),
+
+  body('amount')
+    .optional()
+    .isFloat({ gt: 0 }).withMessage('Amount must be greater than 0'),
+
+  body('suggestedFor')
+    .optional()
+    .isLength({ min: 3 }).withMessage('Suggested For must be at least 3 characters'),
+
+  body('ingredients')
+    .optional()
+    .isLength({ min: 3 }).withMessage('Ingredients must be at least 3 characters'),
+
+  body('stock')
+    .optional()
+    .isInt({ min: 0 }).withMessage('Stock must be 0 or more'),
+
+  body('description')
+    .optional()
+    .isLength({ min: 5 }).withMessage('Description must be at least 5 characters'),
+
+  body('status')
+    .optional()
+    .isIn(['Available', 'Out of Stock'])
+    .withMessage('Status must be Available or Out of Stock'),
+
+  body('action')
+    .optional()
+    .isIn(['Active', 'Inactive'])
+    .withMessage('Action must be Active or Inactive'),
+
+  validate,auth,
+  productController.updateProduct
+);
+
+/**
+ * @swagger
+ * /products/{id}:
+ *   delete:
+ *     summary: Delete product
+ *     tags: [Products]
+ *     security:
+ *       - bearerAuth: []
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *     responses:
+ *       200:
+ *         description: Product deleted
+ *       404:
+ *         description: Product not found
+ *       400:
+ *         description: invalid request
+ *       401:
+ *         description: unauthorized access
+ *       500:
+ *         description: Internal server error
+ */
+router.delete(
+  '/products/:id',
+  param('id').isInt().withMessage('Product ID must be integer'),
+  validate,auth,
+  productController.deleteProduct
+);
+
+/**
+ * @swagger
+ * /products/update-stock/{id}:
+ *   put:
+ *     summary: Update product stock
+ *     tags: [Products]
+ *     security:
+ *       - bearerAuth: []
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             required:
+ *               - qty
+ *             properties:
+ *               qty:
+ *                 type: integer
+ *                 example: 10
+ *     responses:
+ *       200:
+ *         description: Stock updated
+ *       400:
+ *         description: invalid request
+ *       404:
+ *         description: Product not found
+ */
+router.put(
+  '/products/update-stock/:id',
+
+  param('id')
+    .isMongoId().withMessage('Invalid Product ID'),
+
+  body('qty')
+    .notEmpty().withMessage('Quantity is required')
+    .isInt().withMessage('Quantity must be integer'),
+
+  validate,
+  auth,
+  productController.updateStock
+);
+
+module.exports = router;
